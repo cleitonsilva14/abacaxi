527faa924c83b6129736ba520ac5087422f1c5e3
cleitonsilva14 <cleiton18ads@gmail.com>
2024-04-10 14:51:05 -0300

    Implementando a classe AbacaxiService

--- src/main/java/br/com/abacaxi/service/AbacaxiService.java
+++ src/main/java/br/com/abacaxi/service/AbacaxiService.java
@@ -1,8 +1,29 @@
 package br.com.abacaxi.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.abacaxi.entity.Abacaxi;
+import br.com.abacaxi.repository.AbacaxiRepository;
 
 @Service
 public class AbacaxiService {
 
+	@Autowired
+	AbacaxiRepository abacaxiRepository;
+	
+	public List<Abacaxi> getAll(){
+		return abacaxiRepository.findAll();
+	}
+	
+	public Optional<Abacaxi> getOne( Long id){
+			return abacaxiRepository.findById(id);
+	}
+	
+	
+	
+	
 }
